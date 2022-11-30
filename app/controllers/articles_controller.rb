fc0f91cc332c1105d35ced9da2a834fcf5677470
Julien Le Mee <julemee@gmail.com>
2022-11-29 19:16:32 -0500

    find good quality img

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,7 @@ require 'nokogiri'
 
 class ArticlesController < ApplicationController
   def index
-    scrape
+    # scrape
     @articles = Article.all
   end
 
@@ -20,47 +20,47 @@ class ArticlesController < ApplicationController
     @article.save
   end
 
-  def scrape
-    Article.destroy_all if Rails.env.development?
-    articles = []
-    titles = []
-    descriptions = []
-    images = []
-    url = 'https://www.basketusa.com/'
-    html_file = URI.open(url).read
-    doc = Nokogiri::HTML(html_file)
-    doc.search('section.box').each do |element|
-      element.search('.title').each do |title_element|
-        titles << title_element.text.strip
-      end
-      # element.search('.meta-desc').each do |description_element|
-      #   descriptions << description_element.text.strip
-      # end
+  # def scrape
+  #   Article.destroy_all if Rails.env.development?
+  #   articles = []
+  #   titles = []
+  #   descriptions = []
+  #   images = []
+  #   url = 'https://www.basketusa.com/'
+  #   html_file = URI.open(url).read
+  #   doc = Nokogiri::HTML(html_file)
+  #   doc.search('section.box').each do |element|
+  #     element.search('.title').each do |title_element|
+  #       titles << title_element.text.strip
+  #     end
+  #     # element.search('.meta-desc').each do |description_element|
+  #     #   descriptions << description_element.text.strip
+  #     # end
 
-      element.search('img').each do |image_element|
-        images << image_element.attribute('data-src').value
-      end
-      # element.search('a').each do |link|
-      #   article_url = link.attribute('href').value
-      #   article_html_file = URI.open(article_url).read
-      #   article_doc = Nokogiri::HTML(article_html_file)
-      #   article_doc.search('p').each do |description_element|
-      #     descriptions << description_element.text.strip
-      #   end
-      # end
-    end
-    titles.each_with_index do |title, index|
-      articles << Article.create!(title: title, description: descriptions[index], image: images[index])
-    end
-        # title = title_element.text.strip
-        # element.search('.meta-desc').each do |description_element|
-        #   description = description_element.text.strip
-        #   articles << Article.create!(title: title, description: description)
+  #     element.search('img').each do |image_element|
+  #       images << image_element.attribute('data-src').value
+  #     end
+  #     # element.search('a').each do |link|
+  #     #   article_url = link.attribute('href').value
+  #     #   article_html_file = URI.open(article_url).read
+  #     #   article_doc = Nokogiri::HTML(article_html_file)
+  #     #   article_doc.search('p').each do |description_element|
+  #     #     descriptions << description_element.text.strip
+  #     #   end
+  #     # end
+  #   end
+  #   titles.each_with_index do |title, index|
+  #     articles << Article.create!(title: title, description: descriptions[index], image: images[index])
+  #   end
+  #       # title = title_element.text.strip
+  #       # element.search('.meta-desc').each do |description_element|
+  #       #   description = description_element.text.strip
+  #       #   articles << Article.create!(title: title, description: description)
 
-    articles.each do |article|
-      article.destroy if article.title == 'Toute l’info NBA en continu'
-    end
-  end
+  #   articles.each do |article|
+  #     article.destroy if article.title == 'Toute l’info NBA en continu'
+  #   end
+  # end
 
   private
 
